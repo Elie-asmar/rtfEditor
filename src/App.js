5c9e2822eed56f929715428cd1d4c6eefc6ebbfd
Tony Tannous <toni.tannous@ctsjounieh.com>
2022-10-18 13:58:48 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,85 @@
-import React from 'react';
-import './App.css';
+import axios from "axios";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
 
-
-
-
-import { RichEditComponent } from './RichEditComponent';
+import { RichEditComponent } from "./RichEditComponent";
 
 function App() {
+  const rtfRef = useRef(null);
+  const [mystate, setMystate] = useState({ 'centerid': '1', 'resultid': '5', 'resyear': '2022', base64rtf: null });
+  const DB_LINK = useRef('http://183.183.183.122/CtsWebService_LabRad/DataService.svc/web/');
+
+  const handleSave = useCallback(
+    (s, e) => {
+      e.handled = true;
+      // console.log(s)
+      // console.log(e)
+      // console.log(e.base64);
+      // console.log(e.fileName);
+      // //DocumentFormat.OpenXml
+      // //DocumentFormat.PlainText
+      // //DocumentFormat.Rtf
+      // console.log(e.format); //in DocumentFormat
+      let _data = {
+        centerid: mystate.centerid,
+        resultid: mystate.resultid,
+        resyear: mystate.resyear,
+        payload: e.base64
+      };
+
+      axios({
+        method: "post",
+        url: `${DB_LINK.current}SaveRadResultRtf`,
+        data: JSON.stringify(_data),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+      })
+        .then(response => {
+          console.log('Save Successfully')
+
+
+
+        })
+        .catch(error => {
+
+        });
+
+
+    }, [mystate]);
+
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: `${DB_LINK.current}GetRadResultRtf?centerid=1&resultid=5&resyear=2022`,
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then(response => {
+
+        rtfRef.current.rich.openDocument(response.data, 'DocumentName', rtfRef.current.DocFormat.Rtf);
+      })
+      .catch(error => {
+        console.log('error', error)
+
+      });
+  }, []);
+
+
+
+
   return (
-    <RichEditComponent />
+    <>
+      <button onClick={() => {
+        rtfRef.current.rich.saveDocument(rtfRef.current.DocFormat.Rtf)
+
+      }}>Trigger Save From Outer Component</button>
+      <RichEditComponent ref={rtfRef} handleSave={handleSave} />
+    </>
   );
 }
 
 export default App;
-
--- src/RichEditComponent.jsx
+++ src/RichEditComponent.jsx
@@ -5,126 +5,95 @@ import {
     create, createOptions, RichEdit, ViewType, RichEditUnit,
     DocumentFormat, RibbonTabType, FileTabItemId, HomeTabItemId,
 } from 'devexpress-richedit';
-import axios from 'axios';
-const DB_LINK = 'http://183.183.183.122/CtsWebService_LabRad/DataService.svc/web/'
+
+
 class RichEditComponent extends React.Component {
 
     constructor(props) {
         super(props)
         this.rich = RichEdit;
         this.created = false
-
+        this.DocFormat = DocumentFormat
+        this.options = createOptions();
 
     }
 
+
+
     componentDidMount() {
+        console.log('this.props.handleSave', this.props.handleSave)
 
         // the createOptions() method creates an object that contains RichEdit options initialized with default values
-        const options = createOptions();
 
-        let FilesTab = options.ribbon.getTab(RibbonTabType.File);
-        options.ribbon.removeTab(FilesTab)
+
+        let FilesTab = this.options.ribbon.getTab(RibbonTabType.File);
+        this.options.ribbon.removeTab(FilesTab)
         // FilesTab.removeItem(FileTabItemId.OpenDocument)
         // FilesTab.removeItem(FileTabItemId.CreateNewDocument)
         // FilesTab.removeItem(FileTabItemId.ExportDocument)
         // FilesTab.removeItem(FileTabItemId.Download)
-        let ViewTab = options.ribbon.getTab(RibbonTabType.View);
-        options.ribbon.removeTab(ViewTab);
-        let PageLayoutTab = options.ribbon.getTab(RibbonTabType.PageLayout);
-        options.ribbon.removeTab(PageLayoutTab);
-        let ReferencesTab = options.ribbon.getTab(RibbonTabType.References);
-        options.ribbon.removeTab(ReferencesTab);
-        let MailTab = options.ribbon.getTab(RibbonTabType.MailMerge);
-        options.ribbon.removeTab(MailTab);
+        let ViewTab = this.options.ribbon.getTab(RibbonTabType.View);
+        this.options.ribbon.removeTab(ViewTab);
+        let PageLayoutTab = this.options.ribbon.getTab(RibbonTabType.PageLayout);
+        this.options.ribbon.removeTab(PageLayoutTab);
+        let ReferencesTab = this.options.ribbon.getTab(RibbonTabType.References);
+        this.options.ribbon.removeTab(ReferencesTab);
+        let MailTab = this.options.ribbon.getTab(RibbonTabType.MailMerge);
+        this.options.ribbon.removeTab(MailTab);
 
 
 
 
 
-        options.bookmarks.visibility = true;
-        options.bookmarks.color = '#ff0000';
+        this.options.bookmarks.visibility = true;
+        this.options.bookmarks.color = '#ff0000';
 
-        options.confirmOnLosingChanges.enabled = true;
-        options.confirmOnLosingChanges.message = 'Are you sure you want to perform the action? All unsaved document data will be lost.';
+        this.options.confirmOnLosingChanges.enabled = true;
+        this.options.confirmOnLosingChanges.message = 'Are you sure you want to perform the action? All unsaved document data will be lost.';
 
-        options.fields.updateFieldsBeforePrint = true;
-        options.fields.updateFieldsOnPaste = true;
+        this.options.fields.updateFieldsBeforePrint = true;
+        this.options.fields.updateFieldsOnPaste = true;
 
-        options.mailMerge.activeRecord = 2;
-        options.mailMerge.viewMergedData = true;
-        options.mailMerge.dataSource = [
+        this.options.mailMerge.activeRecord = 2;
+        this.options.mailMerge.viewMergedData = true;
+        this.options.mailMerge.dataSource = [
             { Name: 'Indy', age: 32 },
             { Name: 'Andy', age: 28 },
         ];
 
         // events
-        options.events.activeSubDocumentChanged = () => { };
-        options.events.autoCorrect = () => { };
-        options.events.calculateDocumentVariable = () => { };
-        options.events.characterPropertiesChanged = () => { };
-        options.events.contentInserted = () => { };
-        options.events.contentRemoved = () => { };
-        options.events.documentChanged = () => { };
-        options.events.documentFormatted = () => { };
-        options.events.documentLoaded = () => { };
-        options.events.gotFocus = () => { };
-        options.events.hyperlinkClick = () => { };
-        options.events.keyDown = () => { };
-        options.events.keyUp = () => { };
-        options.events.paragraphPropertiesChanged = () => { };
-        options.events.lostFocus = () => { };
-        options.events.pointerDown = () => { };
-        options.events.pointerUp = () => { };
-        options.events.saving = (s, e) => {
-            e.handled = true;
-            console.log(s)
-            console.log(e)
-            console.log(e.base64);
-            console.log(e.fileName);
-            //DocumentFormat.OpenXml
-            //DocumentFormat.PlainText
-            //DocumentFormat.Rtf
-            console.log(e.format); //in DocumentFormat
-            let _data = {
-                centerid: '1',
-                resultid: '5',
-                resyear: '2022',
-                payload: e.base64
-            };
+        this.options.events.activeSubDocumentChanged = () => { };
+        this.options.events.autoCorrect = () => { };
+        this.options.events.calculateDocumentVariable = () => { };
+        this.options.events.characterPropertiesChanged = () => { };
+        this.options.events.contentInserted = () => { };
+        this.options.events.contentRemoved = () => { };
+        this.options.events.documentChanged = () => { };
+        this.options.events.documentFormatted = () => { };
+        this.options.events.documentLoaded = () => { };
+        this.options.events.gotFocus = () => { };
+        this.options.events.hyperlinkClick = () => { };
+        this.options.events.keyDown = () => { };
+        this.options.events.keyUp = () => { };
+        this.options.events.paragraphPropertiesChanged = () => { };
+        this.options.events.lostFocus = () => { };
+        this.options.events.pointerDown = () => { };
+        this.options.events.pointerUp = () => { };
 
-            axios({
-                method: "post",
-                url: `${DB_LINK}SaveRadResultRtf`,
-                data: JSON.stringify(_data),
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json"
-                },
-            })
-                .then(response => {
-
-
-
-                })
-                .catch(error => {
-
-                });
-
-
-        };
-        options.events.saved = (s, e) => {
-            e.handled = true;
-            console.log(s)
-            console.log(e)
-            console.log(e.base64);
-            console.log(e.fileName);
-            //DocumentFormat.OpenXml
-            //DocumentFormat.PlainText
-            //DocumentFormat.Rtf
-            console.log(e.format); //in DocumentFormat
-        };
-        options.events.selectionChanged = () => { };
-        options.events.customCommandExecuted = (s, e) => {
+        this.options.events.saving = this.props.handleSave
+        // this.options.events.saved = (s, e) => {
+        //     e.handled = true;
+        //     console.log(s)
+        //     console.log(e)
+        //     console.log(e.base64);
+        //     console.log(e.fileName);
+        //     //DocumentFormat.OpenXml
+        //     //DocumentFormat.PlainText
+        //     //DocumentFormat.Rtf
+        //     console.log(e.format); //in DocumentFormat
+        // };
+        this.options.events.selectionChanged = () => { };
+        this.options.events.customCommandExecuted = (s, e) => {
             switch (e.commandName) {
                 case 'insertEmailSignature':
                     s.document.insertParagraph(s.document.length);
@@ -141,24 +110,24 @@ class RichEditComponent extends React.Component {
             }
         };
 
-        options.unit = RichEditUnit.Inch;
+        this.options.unit = RichEditUnit.Inch;
 
-        options.view.viewType = ViewType.PrintLayout;
-        options.view.simpleViewSettings.paddings = {
+        this.options.view.viewType = ViewType.PrintLayout;
+        this.options.view.simpleViewSettings.paddings = {
             left: 15,
             top: 15,
             right: 15,
             bottom: 15,
         };
-        options.exportUrl = 'https://siteurl.com/api/';
+        // this.options.exportUrl = 'https://siteurl.com/api/';
 
-        options.readOnly = false;
-        options.width = '1400px';
-        options.height = '400px';
-        options.authentication = "1234"
+        this.options.readOnly = false;
+        this.options.width = '1400px';
+        this.options.height = '400px';
+        this.options.authentication = "1234"
 
         if (!this.created) {
-            this.rich = create(document.getElementById("richEdit"), options);
+            this.rich = create(document.getElementById("richEdit"), this.options);
             this.created = true
         }
 
@@ -167,31 +136,6 @@ class RichEditComponent extends React.Component {
     render() {
         return (
             <>
-                <button onClick={() => {
-                    axios({
-                        method: "get",
-                        url: `${DB_LINK}GetRadResultRtf?centerid=1&resultid=5&resyear=2022`,
-                        headers: {
-                            Accept: "application/json",
-                        },
-                    })
-                        .then(response => {
-
-                            this.rich.openDocument(response.data, 'DocumentName', DocumentFormat.Rtf);
-                            console.log(this.rich.document)
-
-                        })
-                        .catch(error => {
-
-                        });
-
-                }}>Get Data</button>
-                <button onClick={() => {
-                    this.rich.saveDocument(DocumentFormat.Rtf)
-
-
-
-                }}>Save Data</button>
                 <div id="richEdit"></div>
             </>
 
